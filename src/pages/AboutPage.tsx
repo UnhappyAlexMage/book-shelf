ed5e67b854d5aaddb875df2a0a826d42b8d93464
UnhappyAlexMage <alexandr.bro03@gmail.com>
2026-07-07 03:54:27 +0500

    Fixed components MapPage and AboutPage

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -15,55 +15,78 @@ export default function AboutPage() {
 
     return (
         <main className="flex-1 overflow-y-auto bg-white transition-all duration-300 h-[calc(100vh-4.5rem)]">
-            <section className="flex border-l border-slate-200 pt-10">
-                <div className="flex flex-col w-full h-full justify-between items-start pt-10 pl-10 pr-10 pb-2.5 bg-slate-50">
-                    <h3  className="text-8xl font-bold mb-6">Далее представлена краткая информаци о выполненном проекте:</h3>
+            <section className="flex border-l border-slate-200">
+                <div className="flex flex-col w-full h-full justify-between items-start pt-10 pl-10 pr-10 pb-2.5 ">
+                    <h3 className="text-9xl font-bold mb-6">Далее представлена краткая информаци о выполненном проекте:</h3>
                     <nav className="flex gap-4 p-4 bg-slate-100 rounded-lg w-full mb-4">
-                        <Link to="#mission" className="text-blue-600 hover:underline font-medium">
-                            🎯 Наша миссия
+                        <Link to="#technologystack" className="text-blue-600 hover:underline font-medium">
+                            1.Используемый стэк технологий
                         </Link>
-                        <Link to="#team" className="text-blue-600 hover:underline font-medium">
-                            👥 Наша команда
+                        <Link to="#maintasks" className="text-blue-600 hover:underline font-medium">
+                            2.Основные задачи проекта
+                        </Link>
+                        <Link to="#solvedproblems" className="text-blue-600 hover:underline font-medium">
+                            3.Решенные задачи в проекте
                         </Link>
                     </nav>
-                    <p className="text-slate-600 w-full">
-                        Добро пожаловать на страницу "Информация о проекте"! 
-                        Ниже вы можете ознакомиться с некоторым кратко описанным функционалом проекта
-                        Используйте ссылки выше для быстрой навигации.
+                    <p className="text-black w-full">
+                        Добро пожаловать на страницу «Информация о проекте»! 
+                        Ниже вы можете ознакомиться с некоторым кратко описанным функционалом проекта.
+                        Используйте ссылки выше ↑ для быстрой навигации.
                     </p>
                 </div>
             </section>
-            
-            <section id="mission" className="p-10 border-t border-slate-200 min-h-[80vh] bg-slate-50">
-                <h2 className="text-2xl font-bold mb-4 text-slate-800">🎯 Наша миссия</h2>
-                <div className="text-slate-600 space-y-4 max-w-3xl">
-                    <p>
-                        Мы стремимся создать идеальную экосистему для разработчиков, объединяя современные 
-                        технологии и интуитивно понятный дизайн. Наша главная цель — автоматизировать рутинные 
-                        процессы, чтобы вы могли сфокусироваться на создании действительно важных вещей.
+            <section id="technologystack" className="p-10 border-t w-full border-slate-200 min-h-[50vh] bg-slate-200">
+                <h4 className="text-l font-bold mb-4 text-slate-800">Технологический стэк для разработки проекта</h4>
+                <div className="text-black space-y-4 ">
+                    <p className='text-slate-600 w-full'>
+                        Для данного проекта использовался следующий технологический стэк, взятый из требовий к реализации проекта:
                     </p>
-                    <p>
-                        Каждый день мы анализируем отзывы пользователей, оптимизируем производительность 
-                        и добавляем новые функции. Мы верим, что открытый код и сильное сообщество способны 
-                        изменить подход к веб-разработке во всем мире.
+                    <ul className='space-y-2.5 pl-1 text-sm font-medium text-slate-600 list-disc list-inside'>
+                        <li>TailwindCSS</li>
+                        <li>Axios</li>
+                        <li>React/Vite</li>
+                        <li>React Router</li>
+                        <li>Zustand</li>
+                        <li>Yup + React Hook Form</li>
+                        <li>React Icons</li>
+                    </ul>
+                    <p className='text-slate-600 w-full'>
+                        В дальнейшей реализации проекта, будут использованы следующие технологии: добавление базы данных (PostgreSQL) - хранение и сохранение данных,
+                        Node.js (Для разработки полноценной backend-части приложения),
+                        библиотека react-hot-toast при успешных завершенных действия.
                     </p>
                 </div>
             </section>
-
-            {/* ЯКОРЬ 2: Наша команда */}
-            <section id="team" className="p-10 border-t border-slate-200 min-h-[80vh]">
-                <h2 className="text-2xl font-bold mb-4 text-slate-800">👥 Наша команда</h2>
-                <div className="text-slate-600 space-y-4 max-w-3xl">
-                    <p>
-                        За каждым успешным решением стоит команда профессионалов, увлеченных своим делом. 
-                        В нашей студии работают инженеры, дизайнеры и продуктовые аналитики с опытом работы 
-                        в крупнейших IT-компаниях мира.
+            <section id="maintasks" className="p-10 border-t border-slate-200 min-h-[50vh]">
+                <h4 className="text-l font-bold mb-4 text-slate-800">Основные задачи проекта</h4>
+                <div className="text-slate-600 space-y-4">
+                    <p className='text-slate-600 w-full'>
+                        Основные задачи при реализации проекта заключались в следующем: 
                     </p>
-                    <p>
-                        Мы не просто пишем код — мы создаем пользовательский опыт. Каждый интерфейс проходит 
-                        строгое тестирование, а архитектурные решения обсуждаются всей командой, чтобы 
-                        гарантировать масштабируемость и безопасность вашего приложения в будущем.
+                    <ul className='space-y-2.5 pl-1 text-sm font-medium text-slate-600 list-disc list-inside'>
+                        <li>Данные замоканы в файлах проекта</li>
+                        <li>Отсутствие inline-стилей</li>
+                        <li>Адаптивная верстка</li>
+                        <li>Компоненты содержат все виды ссылок: "Минимум 2 якорные ссылки", "Минимум 2 страницы (включая главную страницу)", "По 1 ссылке на телефон, email и внешний ресурс"</li>
+                        <li>Наполнение согласно тематики сайта, содержащие теги в тех. задании.</li>
+                    </ul>
+                    <p className='text-slate-600 w-full'>
+                        Все задачи были выполнены в соответствии с техническими требованиями к проекту.
                     </p>
+                </div>
+            </section>
+            <section id="solvedproblems" className="p-10 border-t border-slate-200 min-h-[50vh] bg-slate-200">
+                <h2 className="text-2xl font-bold mb-4 text-slate-800">Решенные проблемы в течение работы</h2>
+                <div className="text-slate-600 space-y-4">
+                    <p>
+                        В данном проекте были решены следующие проблемы:
+                    </p>
+                    <ul className='space-y-2.5 pl-1 text-sm font-medium text-slate-600 list-disc list-inside'>
+                        <li>Продумывание логики проекта в соответствии с тех. требованиями</li>
+                        <li>Разработка концепции, создание дизайна</li>
+                        <li>Реализация проекта</li>
+                    </ul>
                 </div>
             </section>
         </main>
--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,7 +1,96 @@
+import { useEffect, useRef, useState } from 'react';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import 'ol/ol.css';
 
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point'
+import Polygon from 'ol/geom/Polygon';
 
-export default function MapPage() {
+import { Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
+import { fromLonLat } from 'ol/proj';
+
+import { CheckBoxForMap } from '../shared/ui/CheckBoxForMap';
+
+interface AMarkerPoints {
+    name: string;
+    coords: number[];
+};
+
+const markerPoints: AMarkerPoints[] = [
+    { name: 'Площадь Революции', coords: [61.4025, 55.1598] },
+    { name: 'Кировка', coords: [61.4007, 55.1632] },
+    { name: 'Парк Гагарина', coords: [61.3785, 55.1610] }
+];
+
+export default function MapPage () {
+    const mapRef = useRef(null);
+    const [showPoints, setShowPoints] = useState(true);
+    const [showPolygons, setShowPolygons] = useState(true);
+
+    useEffect(() => {
+        if (!mapRef.current) return;
+
+        const chelyabinskMarker = markerPoints.map(loc => {
+            return new Feature({
+                geometry: new Point(fromLonLat(loc.coords))
+            });
+        });
+        const pointsLayer = new VectorLayer({
+            source: new VectorSource({ features: chelyabinskMarker }),
+            visible: showPoints,
+        });
+
+        const areaPolygon = new Feature({
+            geometry: new Polygon([[
+                fromLonLat([61.25, 55.10]), // Левый нижний угол
+                fromLonLat([61.55, 55.10]), // Правый нижний угол
+                fromLonLat([61.55, 55.22]), // Правый верхний угол
+                fromLonLat([61.25, 55.22]), // Левый верхний угол
+                fromLonLat([61.25, 55.10]), // Замыкаем полигон
+            ]]),
+        });
+        const polygonsLayer = new VectorLayer({
+            source: new VectorSource({ features: [areaPolygon] }),
+            visible: showPolygons
+        });
+
+        const map = new Map({
+            target: mapRef.current,
+            layers: [
+                new TileLayer({ source: new OSM() }),
+                polygonsLayer,
+                pointsLayer
+            ],
+            view: new View({
+                center: fromLonLat([61.4368, 55.1549]),
+                zoom: 10,
+            }),
+        });
+
+        return () => map.setTarget(undefined);
+    }, [showPoints, showPolygons]);
+
     return (
-    <div className="p-8">Компонент Карты (без сайдбара)</div> 
-    )
+        <div className="p-10 h-full w-full">
+        <div className="flex mb-3 gap-5">
+            <CheckBoxForMap 
+                label="Показать точки"
+                type="checkbox"
+                checked={showPoints}
+                onChange={(e) => setShowPoints(e.target.checked)}
+            />
+            <CheckBoxForMap 
+                label="Показать полигоны"
+                type="checkbox"
+                checked={showPolygons}
+                onChange={(e) => setShowPolygons(e.target.checked)}
+            />
+        </div>
+            <div ref={mapRef} className="w-full h-96" />
+        </div>
+    );
 };
