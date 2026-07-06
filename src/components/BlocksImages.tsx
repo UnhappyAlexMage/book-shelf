62c720fe74d4e685d66e5f34830b514c0d8daac7
UnhappyAlexMage <alexandr.bro03@gmail.com>
2026-07-06 23:57:31 +0500

    Added component BlocksImages

---
+++ src/components/BlocksImages.tsx
@@ -0,0 +1,38 @@
+import type { LibraryBook } from "../entities/types";
+
+import defaultBookCover from '../assets/defaultBookCover.jpg';
+
+interface PropsBlocksImages {
+    selectedbook: LibraryBook;
+};
+
+export default function BlocksImages (props: PropsBlocksImages) {
+    return(
+            <article className='w-[40%] h-full overflow-y-auto bg-white p-6 flex justify-center'>
+                <div className="rounded-2xl border border-slate-100 bg-slate-100 p-4">
+                    <h3 className="text-xs font-semibold uppercase tracking-wider text-slate-400 mb-3">
+                        Так же просматривают: 
+                    </h3>
+                    <div className="grid grid-cols-4 gap-2">
+                        {props.selectedbook.reccomendedImages?.map((imgUrl, index) => (
+                            <div 
+                                key={index} 
+                                className="aspect-3/4 overflow-hidden rounded-lg border border-slate-200 bg-white cursor-pointer hover:border-sky-500 transition-colors"
+                            >
+                                <img 
+                                    src={imgUrl} 
+                                    alt={`Обложка ${index + 1}`} 
+                                    className="h-full w-full object-cover"
+                                    onError={(error) => {
+                                        if (error.currentTarget.src !== defaultBookCover) {
+                                            error.currentTarget.src = defaultBookCover;
+                                        }
+                                    }}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </article>
+    )
+};
