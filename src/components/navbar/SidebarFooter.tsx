5b756c2d4b47b994f82d920f2e296700297555a5
UnhappyAlexMage <alexandr.bro03@gmail.com>
2026-07-06 23:56:13 +0500

    Fixed components SidebarNav and SidebarFooter

--- src/components/navbar/SidebarFooter.tsx
+++ src/components/navbar/SidebarFooter.tsx
@@ -1,48 +1,56 @@
+import React from "react";
 
-
+import { useAuthStore } from "../../store/useAuthUser";
 import { FiLogOut, FiMail, FiPhone, FiGithub } from "react-icons/fi";
+import type { AContacsLink } from "../../entities/typesUI";
+
+import { ContacsLink } from "../../shared/ui/ContacsLink";
+
+const dataContacs: AContacsLink[] = [
+    {label: "+7(996)123-45-67", href: "tel:+7-996-123-45-67", icon: FiPhone}, 
+    {label: "Gmail Author", href: "mailto:alexandr.bro03@gmail.com", target: "blank" ,icon: FiMail},
+    {label: "GitHub Repository", href: "https://github.com/UnhappyAlexMage", target: "_blank", rel: "noopener noreferrer", icon: FiGithub}
+];
 
 export default function SidebarFooter() {
+    const user = useAuthStore((state) => state.user);
+    const logoutInStore = useAuthStore((state) => state.logout);
+
+    const handleClick = (event: any) => {
+        event?.preventDefault();
+        logoutInStore();
+    };
+
     return (
         <footer className="mt-auto min-h-3/11 border-t border-gray-200 bg-white px-6 py-5">
-
             <div className="space-y-3">
-                <a
-                    href="tel:+79991234567"
-                    className="flex items-center gap-3 text-sm text-gray-500 transition-colors hover:text-sky-600"
-                >
-                    <FiPhone className="shrink-0 text-lg" />
-                    <span>+7 (999) 123-45-67</span>
-                </a>
-                <a
-                    href="mailto:booknest@gmail.com"
-                    className="flex items-center gap-3 text-sm text-gray-500 transition-colors hover:text-sky-600"
-                >
-                    <FiMail className="shrink-0 text-lg" />
-                    <span className="truncate">
-                        booknest@gmail.com
-                    </span>
-                </a>
-                <a
-                    href="https://github.com/your-username/booknest"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="flex items-center gap-3 text-sm text-gray-500 transition-colors hover:text-sky-600"
-                >
-                    <FiGithub className="shrink-0 text-lg" />
-                    <span className="truncate">
-                        GitHub Repository
-                    </span>
-                </a>
-            </div>
-            <button
-                className="mt-6 flex w-full items-center justify-center gap-2 rounded-xl border border-red-600 py-2.5
-                text-sm font-medium text-red-500 hover:text-red-700"
-            >
-            <FiLogOut className="text-lg" />
-                Выйти 
-            </button>
+                {dataContacs.map((contact) => (
+                    <ContacsLink 
+                        label={contact.label}
+                        href={contact.href}
+                        icon={contact.icon}
+                        target={contact.target}
+                        rel={contact.rel}
+                    />
+                ))}
 
+            </div>
+            <React.Fragment>
+                {(user === null) ? (
+                    <div className="mt-6 text-center text-sm outline-1">
+                        Перейдите на страницу авторизации для входа
+                    </div>
+                ) : (                
+                    <button
+                        onClick={handleClick}
+                        className="mt-6 flex w-full items-center justify-center gap-2 rounded-xl border border-red-600 py-2.5
+                        text-sm font-medium text-red-500 hover:text-red-700"
+                    >
+                    <FiLogOut className="text-lg" />
+                        Выйти 
+                    </button>
+                )}
+            </React.Fragment>
         </footer>
     );
 }
--- src/components/navbar/SidebarNav.tsx
+++ src/components/navbar/SidebarNav.tsx
@@ -11,6 +11,7 @@ import React from "react";
 
 import type { ATypesNav } from "../../entities/typesUI";
 import type { BookStatus } from "../../entities/types";
+import { NavLink } from "react-router";
 
 interface PropsSidevarNav {
     currentStatusSidebar: BookStatus;
@@ -18,17 +19,18 @@ interface PropsSidevarNav {
 };
 
 let menuItems: ATypesNav[] = [
-    { id: 1, current: "Все книги", label: "Все книги", icon: PiBooksBold, uniqueColorText: 'text-sky-600', uniqueColorBG: 'bg-sky-600', bookStatus: 'Все книги'},
-    { id: 2, current: "Чтение", label: "Чтение", icon: FaReadme, uniqueColorText: 'text-sky-600', uniqueColorBG: 'bg-teal-500', bookStatus: 'Чтение'},
-    { id: 3, current: "Прочитанные", label: "Прочитанные", icon: AiOutlineFileDone, uniqueColorText: 'text-sky-600', uniqueColorBG: 'bg-lime-500', bookStatus: 'Прочитанные'},
-    { id: 4, current: "Фавориты", label: "Фавориты", icon: MdRecommend, uniqueColorText: 'text-sky-600', uniqueColorBG: 'bg-purple-500', bookStatus: 'Фавориты' },
-    { id: 5, current: "Ваши записи", label: "Ваши записи", icon: AiOutlineUserSwitch, uniqueColorText: 'text-yellow-600', uniqueColorBG: 'bg-green-500', padding: 'mt-8'},
-    { id: 6, current: "Карта", label: "Карта", icon: LuMapPinCheck, uniqueColorText: 'text-green-700', uniqueColorBG: 'bg-green-500'},
-    { id: 7, current: "О проекте", label: "О проекте", icon: AiOutlineExclamationCircle, uniqueColorText: 'text-red-500', uniqueColorBG: 'bg-gray-600',},
+    { id: 1, current: "Все книги", label: "Все книги", icon: PiBooksBold, uniqueColorText: 'text-sky-600', bookStatus: 'Все книги'},
+    { id: 2, current: "Чтение", label: "Чтение", icon: FaReadme, uniqueColorText: 'text-sky-600', bookStatus: 'Чтение'},
+    { id: 3, current: "Прочитанные", label: "Прочитанные", icon: AiOutlineFileDone, uniqueColorText: 'text-sky-600', bookStatus: 'Прочитанные'},
+    { id: 4, current: "Фавориты", label: "Фавориты", icon: MdRecommend, uniqueColorText: 'text-sky-600', bookStatus: 'Фавориты' },
+    { id: 5, current: "Ваши записи", label: "Ваши записи", icon: AiOutlineUserSwitch, uniqueColorText: 'text-yellow-600', padding: 'mt-8', bookStatus: 'Ваши записи'},
+    { id: 6, current: "Карта", label: "Карта", icon: LuMapPinCheck, uniqueColorText: 'text-green-700', toLink: '/map'},
+    { id: 7, current: "О проекте", label: "О проекте", icon: AiOutlineExclamationCircle, uniqueColorText: 'text-red-500', toLink: '/about'},
 ];
 
 export default function SidebarNav(props: PropsSidevarNav) {
     const [isCorrect, setIsCorrect] = useState<string>('Все книги');
+    
 
     return(
         <nav className="flex-1 overflow-y-auto space-y-2 bg-white">
@@ -36,32 +38,44 @@ export default function SidebarNav(props: PropsSidevarNav) {
                 {menuItems.map((menuItem) => (
                     <React.Fragment key={menuItem.id}>
                         {menuItem.id === 6 && (
-                            <span className="mt-4 mb-2 text-sm font-semibold uppercase tracking-wide text-gray-400 border-t border-gray-200">
-                                <h3 className="text-sm text-slate-800 ml-4 mt-4">Другое</h3>
-                            </span>
-                        )}
-                        <button
-                            // onClick={() => props.setStatusSidebar(menuItem.current), () => setIsCorrect(menuItem.label)}
-                            onClick={() => { setIsCorrect(menuItem.current)
-                                if(menuItem.bookStatus) {
-                                    props.setStatusSidebar(menuItem.bookStatus)
-                                }
-                            }}
-                            className={`flex justify-between w-full h-12 items-center text-2xl 
-                                ${menuItem.padding || ""}
-                                ${ isCorrect === menuItem.current ? "bg-gray-200 font-semibold" : "text-gray-600 bg-transparent" }`}
-                        >
-                            <div className="flex items-center ml-4 p-1">
-                                <menuItem.icon
-                                    className={`text-3xl text-gray-400 ${ isCorrect === menuItem.current ? menuItem.uniqueColorText : "" }`}
-                                />
-                                <span
-                                    className={`pl-3 pb-1 ${ isCorrect === menuItem.current ? menuItem.uniqueColorText : "text-black-400" }`}
-                                >
-                                    {menuItem.current}
+                                <span className="mt-4 mb-2 text-sm font-semibold uppercase tracking-wide text-gray-400 border-t border-gray-200">
+                                    <h3 className="text-sm text-slate-800 ml-4 mt-4">Другое</h3>
                                 </span>
-                            </div>
-                        </button>
+                        )}
+                        {menuItem.toLink ? (
+                            <NavLink
+                                to={menuItem.toLink}
+                                onClick={() => setIsCorrect(menuItem.current)}
+                            >
+                                <div className="flex items-center ml-4 p-2">
+                                    <menuItem.icon className={`text-3xl text-gray-400 ${isCorrect === menuItem.current ? menuItem.uniqueColorText : ""}`} />
+                                    <span className={`pl-3 pb-1 ${isCorrect === menuItem.current ? menuItem.uniqueColorText : "text-black-400"}`}>
+                                        {menuItem.current}
+                                    </span>
+                                </div>
+                            </NavLink>
+                                ) : (
+                            <button
+                                // onClick={() => props.setStatusSidebar(menuItem.current), () => setIsCorrect(menuItem.label)}
+                                onClick={() => { setIsCorrect(menuItem.current)
+                                    if(menuItem.bookStatus) {
+                                        props.setStatusSidebar(menuItem.bookStatus)
+                                    }
+                                }}
+                                className={`flex justify-between w-full h-12 items-center text-2xl 
+                                    ${menuItem.padding || ""}
+                                    ${ isCorrect === menuItem.current ? "bg-gray-200 font-semibold" : "text-gray-600 bg-transparent" }`}
+                            >
+                                <div className="flex items-center ml-4 p-1">
+                                    <menuItem.icon className={`text-3xl text-gray-400 ${ isCorrect === menuItem.current ? menuItem.uniqueColorText : "" }`} />
+                                    <span
+                                        className={`pl-3 pb-1 ${ isCorrect === menuItem.current ? menuItem.uniqueColorText : "text-black-400" }`}
+                                    >
+                                        {menuItem.current}
+                                    </span>
+                                </div>
+                            </button>
+                        )}
                     </React.Fragment>
                 ))}
             </section>
