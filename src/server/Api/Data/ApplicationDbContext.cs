d99be9fca26538496883ef89c113e0c72b9207ad
UnhappyAlexMage <alexandr.bro03@gmail.com>
2026-07-23 04:36:08 +0500

    Added db context and factory

---
+++ src/server/Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Api.Data.Models;
+
+namespace Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ src/server/Api/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Api.Settings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data
+{
+    // Этот класс будет вызываться ситемой миграций принудительно, в обход Program.cs
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            // Собираем конфигурацию, которая умеет заглядывать в секреты пользователя
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .Build();
+
+            var appSettings = new AppSettings();
+            configuration.Bind(appSettings);
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseNpgsql(appSettings.Db.ConnectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
