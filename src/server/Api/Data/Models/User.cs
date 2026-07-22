a6cc187b6a3a2eece7da390479a2684fd7cf36fb
UnhappyAlexMage <alexandr.bro03@gmail.com>
2026-07-23 04:34:21 +0500

    Added model User

---
+++ src/server/Api/Data/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Data.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [StringLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [StringLength(100)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
